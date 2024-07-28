b322d88376f13d0c7d69990544f319bd994a2b24
manishkharbuja389 <manish.kharbuja389@gmail.com>
2024-07-28 21:01:28 +0545

    1st commit

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import CV from "../../assets/my-image2.jpg";
+
+export default function CTA() {
+  return (
+    <div className="cta">
+      {/* <a href={CV} download className="btn">
+        Download
+      </a> */}
+      {/* <a className="btn">Download CV</a> */}
+      <a href="#contacts" className="btn btn-primary">
+        Contact Me
+      </a>
+    </div>
+  );
+}
